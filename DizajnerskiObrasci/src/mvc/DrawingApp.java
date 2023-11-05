5cccd8f2224fa5466fab10d9cf9965293df9de6a
PavleRogan <pavle.rogan15@gmail.com>
2023-11-05 12:09:11 +0100

    Initial test

--- DizajnerskiObrasci/src/mvc/DrawingApp.java
+++ DizajnerskiObrasci/src/mvc/DrawingApp.java
@@ -3,7 +3,7 @@ package mvc;
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		System.out.println("Dobrodosli na vezbe iz predmeta Dizajnerski obrasci.");
+		System.out.println("Projekat iz predmeta Dizajnerski Obrasci - IT5-2020");
 	}
 
 }
